211c569295571b2f0b170531d728e996c01b5790
AditiSh88 <151556876+AditiSh88@users.noreply.github.com>
2026-04-20 03:27:18 +0530

    Update 2_Insights.py

--- app/pages/2_Insights.py
+++ app/pages/2_Insights.py
@@ -10,7 +10,6 @@ metrics = json.load(open(os.path.join(BASE_DIR, "models/metrics.json")))
 
 st.title("Model Insights")
 
-# ---------- METRICS ----------
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("Accuracy", metrics["accuracy"])
 col2.metric("Precision", metrics["precision"])
@@ -25,7 +24,7 @@ y_pred = [0,1,0,1,0,0,0,1]
 
 st.subheader("Confusion Matrix")
 
-fig, ax = plt.subplots(figsize=(4,4))
+fig, ax = plt.subplots(figsize=(2, 2))
 ConfusionMatrixDisplay(confusion_matrix(y_true, y_pred)).plot(ax=ax)
 st.pyplot(fig)
 
@@ -36,9 +35,8 @@ st.subheader("ROC Curve")
 fpr, tpr, _ = roc_curve(y_true, y_pred)
 roc_auc = auc(fpr, tpr)
 
-fig2, ax2 = plt.subplots(figsize=(5,3))
+fig2, ax2 = plt.subplots(figsize=(3, 2)
 ax2.plot(fpr, tpr, label=f"AUC={roc_auc:.2f}")
 ax2.plot([0,1],[0,1])
 ax2.legend()
-
 st.pyplot(fig2)
