c3c3925fc1c89aa050f0e61ca8d521b52f26cf7d
AditiSh88 <151556876+AditiSh88@users.noreply.github.com>
2026-04-20 02:53:20 +0530

    Update model_loader.py

--- app/utils/model_loader.py
+++ app/utils/model_loader.py
@@ -6,4 +6,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 def load_models():
     tfidf = pickle.load(open(os.path.join(BASE_DIR, "models/tfidf.pkl"), "rb"))
     logreg = pickle.load(open(os.path.join(BASE_DIR, "models/logreg.pkl"), "rb"))
-    return tfidf, logreg
+    xgb = pickle.load(open(os.path.join(BASE_DIR, "models/xgb.pkl"), "rb"))
+    return tfidf, logreg, xgb
